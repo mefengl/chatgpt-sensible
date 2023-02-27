50cc767769a0cd375ca33b673d23b64db347b9ca
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-02-28 00:04:37 +0800

    feat(newchat): init drug down feature

---
+++ script.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         chatgpt sensible
+// @namespace    https://github.com/mefengl
+// @version      0.0.1
+// @description  sensible to me
+// @author       mefengl
+// @match        https://chat.openai.com/chat
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=openai.com
+// @require      https://cdn.staticfile.org/jquery/3.6.1/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $(document).ready(function() {
+        $('nav a:first').appendTo('form div:first');
+    });
+})();
